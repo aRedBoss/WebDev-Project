44d7e6d607dc23cb5bcfd6a20e6ccdccf7178e9e
Omar <aldulaimiomar111@gmail.com>
2025-01-31 13:53:29 +0200

    users MVC DONE

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,4 +1,18 @@
-console.log('hello, world');
+const express = require('express');
+const app = express();
+const userRouter = require('./routes/userRouter');
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Use the userRouter for all /users routes
+app.use('/users', userRouter);
+
+const port = 4000;
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
 
-  
+
