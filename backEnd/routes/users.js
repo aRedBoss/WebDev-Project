a9a1b20bf1e27bb2b1f03f24509cf0e59caf2c20
Omar <aldulaimiomar111@gmail.com>
2025-01-31 13:49:29 +0200

    users MVC DONE

--- backEnd/routes/users.js
+++ backEnd/routes/users.js
@@ -1,5 +1,26 @@
-import Express from 'express';
+const express = require('express');
+const router = express.Router();
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+} = require('../controllers/userControllers');
 
-const app = Express();
+// GET /users
+router.get('/', getAllUsers);
 
-app.post('/',)
+// POST /users
+router.post('/', createUser)
+
+// GET /users/:userId
+router.get('/:userId', getUserById)
+
+// PUT /users/:userId
+router.put('/:userId', updateUser)
+
+// DELETE /users/:userId
+router.delete('/:userId', deleteUser)
+
+module.exports = router;
