47c42a34cd19e69fe5585d8e5b3ddd9317a84758
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-02-01 18:27:01 +0200

    Initial commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,34 @@
-function Navbar() {
+import logo from "../assets/barber-1.png";
+const Navbar = () => {
   return (
-    <div className="p-6 bg-amber-500 underline text-5xl text-center ">
-      Navbar using Tailwind! We can now use Tailwind!
-    </div>
+    <nav className="flex items-center justify-between bg-white shadow-md">
+      <div className=" flex items-center p-4 ml-40 text-2xl">
+        <img src={logo} alt="MUSA Barber Shop" className=" h-12 w-12" />
+      </div>
+      <div className="flex space-x-4">
+        <a href="#home" className="text-gray-800 hover:text-gray-600">
+          Home
+        </a>
+        <a href="#about" className="text-gray-800 hover:text-gray-600">
+          About
+        </a>
+        <a href="#services" className="text-gray-800 hover:text-gray-600">
+          Services
+        </a>
+        <a href="#contact" className="text-gray-800 hover:text-gray-600">
+          Contact
+        </a>
+      </div>
+      <div className="flex space-x-4 mr-40">
+        <button className="px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-700">
+          Sign In
+        </button>
+        <button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-400">
+          Book
+        </button>
+      </div>
+    </nav>
   );
-}
+};
 
 export default Navbar;
