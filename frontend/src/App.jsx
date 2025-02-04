ec0584d1134403313ecd5c16330dc2cb99cd0b7d
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-02-05 00:00:09 +0200

    Update import order

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
+import "./App.css";
 import Navbar from "./components/Navbar";
 import Services from "./components/Services";
 import Hero from "./components/Hero";
-import "./App.css";
-import RegisterPage from "./pages/RegisterPage.jsx";
-import SignInPage from "./pages/SignInPage.jsx";
+import RegisterPage from "./pages/RegisterPage";
+import SignInPage from "./pages/SignInPage";
 
 function App() {
   return (
