73a60a1092ca11f24376ba0206f3fffc72d6b8dc
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-02-07 21:28:52 +0200

    Reorder components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import Services from "./components/Services/Services.jsx";
 import Home from "./components/Home/Home.jsx";
-import Register from "./components/Register/Register.jsx";
-import SignIn from "./components/SignIn/SignIn.jsx";
 import AboutUs from "./components/AboutUs/AboutUs.jsx";
+import Services from "./components/Services/Services.jsx";
 import Contact from "./components/Contact/Contact.jsx";
+import SignIn from "./components/SignIn/SignIn.jsx";
+import Register from "./components/Register/Register.jsx";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
-      <Services />
       <AboutUs />
+      <Services />
       <Contact />
       <SignIn />
       <Register />
