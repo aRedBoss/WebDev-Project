9dfd9f4482f9deb1048a823f51258618306125da
Omar <aldulaimiomar111@gmail.com>
2025-02-09 22:29:21 +0200

    Code refactored to latest version (MongoDB, try-catch)

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const url = '';
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect("mongodb://localhost:27017/web-dev");
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
 
-mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => {
-    console.log("is connected to the db");
-})
-.catch((err) => console.log("Some error", err));
-
-process.on('SIGINT', () => {
-    mongoose.disconnect()
-        .then(() => console.log("db connection closed"))
-        .finally(() => process.exit(0));
-});
+module.exports = connectDB;
